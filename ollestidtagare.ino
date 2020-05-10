6f0fdc2a8df5dac476f6136b48052edefaab30d4
Tobias Jansson <tobias.jansson@gmail.com>
2020-05-10 15:33:28 +0200

    La till konvertering av ms till vettig tid

--- ollestidtagare.ino
+++ ollestidtagare.ino
@@ -41,7 +41,7 @@ void loop() {
     // Avståndet blir tiden * ljudhastigheten delat med två
     int distance = readUltrasonicDistance(27, 27) * 0.340 / 2;
 
-    Serial.println(distance);
+   // Serial.println(distance);
 
     if (startTimer(distance)) {
         timelapse = millis() - start;
@@ -49,6 +49,19 @@ void loop() {
         matrix.writeDisplay();
         Serial.println(timelapse);
         Serial.println(varv);
+
+        // Ok, vi har kört ett varv. Vill skriva ut tiden lite snyggare än i ms
+        byte tid[4];
+        milliSecondsToTime(timelapse, tid);
+
+        Serial.print(tid[0]);
+        Serial.print(":");
+        Serial.print(tid[1]);
+        Serial.print(":");
+        Serial.print(tid[2]);
+        Serial.print(".");
+        Serial.print(tid[3]);
+        Serial.println();
     }
 
     // Blinka lite så vi vet att vi lever
@@ -73,7 +86,27 @@ bool startTimer(int dist) {
         }
     }
     return false;
-    
+}
+
+void milliSecondsToTime(unsigned long ms, byte *tider) {
+    int sec = floor(ms / 1000);
+    int min = floor(sec / 60);
+    int hour = floor(min / 60);
+
+    // Blir weird med sec/min/hour mfl som kan ha värden över 60/60/24, så fixa
+    sec = sec % 60;
+    min = min % 60;
+    hour = hour % 24;
+
+    unsigned long totalsec = hour * 60 * 60 + min * 60 + sec;
+    unsigned long hundradels = round(
+        (ms - totalsec * 1000) / 10);  // Kanske går en omväg men borde funka
+
+    // Vill inte skriva ut här, borde bara vara konv.
+    tider[0] = hour;
+    tider[1] = min;
+    tider[2] = sec;
+    tider[3] = hundradels;
 }
 
 long readUltrasonicDistance(int triggerPin, int echoPin) {
